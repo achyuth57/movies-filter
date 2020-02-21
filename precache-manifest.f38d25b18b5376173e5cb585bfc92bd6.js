2b46004531623e2e0dce98b8d709569260f218a4
Achyuth57 <achyuthkumar57@gmail.com>
2020-02-21 17:59:25 +0530

    Updates

---
+++ precache-manifest.f38d25b18b5376173e5cb585bfc92bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f521ce1bfb3e3168b71b3b3c40426e",
+    "url": "/movies-filter-app/index.html"
+  },
+  {
+    "revision": "5a0b6e42bbdac75a025f",
+    "url": "/movies-filter-app/static/css/main.977db8a1.chunk.css"
+  },
+  {
+    "revision": "c4323f7c2dad3d94c78a",
+    "url": "/movies-filter-app/static/js/2.a087dbb6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movies-filter-app/static/js/2.a087dbb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0b6e42bbdac75a025f",
+    "url": "/movies-filter-app/static/js/main.97979a09.chunk.js"
+  },
+  {
+    "revision": "f7bc26b4b7b7b1841a62",
+    "url": "/movies-filter-app/static/js/runtime-main.1e0cff29.js"
+  }
+]);
